89d2892b92168071221f088952057307f4244bd7
twagh-ias <twagh@integralads.com>
2021-12-13 13:25:26 +0530

    Login auth commit

--- src/main/java/com/auth/login/BcryptPwd.java
+++ src/main/java/com/auth/login/BcryptPwd.java
@@ -5,11 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BcryptPwd {
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        String encodedPassword = passwordEncoder.encode("admin");
-//        System.out.println(encodedPassword);
-
-//        String encodedPassword1 = passwordEncoder.encode("employee");
-//        System.out.println(encodedPassword1);
 
         String encodedPassword3 = passwordEncoder.encode("kpmanager");
         System.out.println(encodedPassword3);
